4e99f2aa855291e80cf11da5a77ba78b1b5710aa
jumbonova <153301977+JumbonovaYT@users.noreply.github.com>
2024-12-19 23:48:43 -0800

    v1.1.0
    those two, once called forbidden, lines of codes are now short
    oh yeah new hide links feature

--- src/main.cpp
+++ src/main.cpp
@@ -1,102 +1,116 @@
 #include <Geode/Geode.hpp>
 using namespace geode::prelude;
 #include <Geode/modify/CommentCell.hpp>
+#include <regex>
 class $modify(CommentCell) {
-
 	void loadFromComment(GJComment* comment) {
-		/*get settings that are fetched multiple times (just to make stuff easier)*/
 		auto logActions = Mod::get()->getSettingValue<bool>("Log actions");
 
-		/*make isHidden bool*/
 		bool isHidden = false;
-
 		auto spambutton = this->getChildByIDRecursive("spam-button"); /*this is why the mod requires 1.14.1 or greater node ids (1.14.1 because it's the latest commentcell update)*/
-
 		if (spambutton) {
 			isHidden = true;
-		} else {
-			isHidden = false;
-		};
+		}
 
 		/*Hide Bot Comments*/
 
 		if (Mod::get()->getSettingValue<bool>("Hide bot comments")) {
-			/*save user id*/
 			auto commenter = comment->m_userID;
-			/*if user id is a known bot id...*/
-			if (commenter == 242664076 /*xh2c*/ || commenter == 164866826 /*3eelolbackup*/) {
+
+			if (commenter == 242664076 /*xh2c*/ || commenter == 164866826 /*3eelolbackup*/) { // i believe both of these people's comments got deleted, so i might delete them next update
 				if (!isHidden) {
-					/*...hide comment*/
 					isHidden = true;
 					comment->m_isSpam = true;
 					if (logActions) {
 						log::debug("found bot comment");
-					};
+					}
 				} else {
-					/*and this opens the comment*/
 					if (logActions) {
 						log::debug("opened bot comment");
-					};
+					}
 					comment->m_isSpam = false;
 					isHidden = false;
-				};
-			};
-		};
+				}
+			}
+		}
 
 		/*Hide Brainrot*/
 
 		if (Mod::get()->getSettingValue<bool>("Hide brainrot comments")) {
-			/*get the comment as lowercase*/
 			std::string commentstring = comment->m_commentString;
 			std::transform(commentstring.begin(), commentstring.end(), commentstring.begin(), tolower);
 
-			/*if the mod contains a brainrot keyword...*/
-			if (commentstring.find("skibidi") != std::string::npos || commentstring.find("skibiti") != std::string::npos || commentstring.find("rizz") != std::string::npos || commentstring.find("mog") != std::string::npos || commentstring.find("gyat") != std::string::npos || commentstring.find("gyaa") != std::string::npos || commentstring.find("ohio") != std::string::npos || commentstring.find("sigma") != std::string::npos || /*sorry to anyone seeing the next 2. they're brainrot so i have to block them*/ commentstring.find("jelq") != std::string::npos || commentstring.find("goon") != std::string::npos || commentstring.find("diddy") != std::string::npos || commentstring.find("mew") != std::string::npos) {
+			std::regex brainrotWords("skibi(d|t)i|rizz|ohio|gya(t|a)|mew | mew|mog|jelq|goon|sigma"); /*this one got way shorter after trying regex, and mew uses spaces because "somewhere" has mew in it*/
+
+			if (std::regex_search(commentstring, brainrotWords)) {
 				if (!isHidden) {
-					/*...hide comment*/
 					isHidden = true;
 					comment->m_isSpam = true;
 					if (logActions) {
 						log::debug("found brainrot comment");
-					};
+					}
 				} else {
-					/*and this opens the comment*/
 					if (logActions) {
 						log::debug("opened brainrot comment");
-					};
+					}
 					comment->m_isSpam = false;
 					isHidden = false;
-				};
-			};
-		};
+				}
+			}
+		}
 		
 		/*Hide Ads*/
 
 		if (Mod::get()->getSettingValue<bool>("Hide ads")) {
-			/*get the comment as lowercase*/
 			std::string commentstring = comment->m_commentString;
 			std::transform(commentstring.begin(), commentstring.end(), commentstring.begin(), tolower);
 
-			/*if the mod contains an ad keyword...*/
-			if (/*saw a comment on retray saying "pls, play 113020262 >:3" so i decided to add these next few keywords*/commentstring.find("play 1") != std::string::npos || commentstring.find("play 2") != std::string::npos || commentstring.find("play 3") != std::string::npos || commentstring.find("play 4") != std::string::npos || commentstring.find("play 5") != std::string::npos || commentstring.find("play 6") != std::string::npos || commentstring.find("play 7") != std::string::npos || commentstring.find("play 8") != std::string::npos || commentstring.find("play 9") != std::string::npos || /*okay now to some other keywords*/ commentstring.find("play this level") != std::string::npos || /*this one was added because their post could say play my level. this one could have some false positives, so i might remove this later*/ commentstring.find("click my") != std::string::npos || /*...and this one*/ commentstring.find("click on my") != std::string::npos || commentstring.find("play my level") != std::string::npos) {
+			std::regex adWords("(play|try) *\\d|(play|try) *(my|this|me) *((recent)?|(new)?|(latest)?) *(est|er)* *(level|lvl)|(click|tap|press|view) *(on *my|my) *(profile|pfp|icon|name)"); // sorry theres kinda a lot
+
+			if (std::regex_search(commentstring, adWords)) {
 				if (!isHidden) {
-					/*...hide comment*/
 					isHidden = true;
 					comment->m_isSpam = true;
 					if (logActions) {
 						log::debug("found ad comment");
-					};
+					}
 				} else {
-					/*and this opens the comment*/
 					if (logActions) {
 						log::debug("opened ad comment");
-					};
+					}
 					comment->m_isSpam = false;
 					isHidden = false;
-				};
-			};
-		};
-	/*and then finally load the comment*/
+				}
+			}
+		}
+
+		/*Hide Links*/
+
+		if (Mod::get()->getSettingValue<bool>("Hide links")) {
+			std::string commentstring = comment->m_commentString;
+			std::transform(commentstring.begin(), commentstring.end(), commentstring.begin(), tolower);
+
+			std::regex linkWords("https*|(web|www)( *dot|\\.)|(\\.|dot) *gg|(\\.|dot) *com|(\\.|dot) *io|(\\.|dot) *org");
+
+			if (std::regex_search(commentstring, linkWords)) {
+				if (!isHidden) {
+					isHidden = true;
+					comment->m_isSpam = true;
+					if (logActions) {
+						log::debug("found comment with a link");
+					}
+				} else {
+					if (logActions) {
+						log::debug("opened comment with a link");
+					}
+					comment->m_isSpam = false;
+					isHidden = false;
+				}
+			}
+		}
 	CommentCell::loadFromComment(comment);
-	};
+	}
 };
+
+/* i can now say i suck at coding :woohoo:
+thanks to erymanthus for telling me to learn regex */
